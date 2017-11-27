0ad8351cb66315cdb6f1c70e780c189c83e2cba0
louisluong <louisluong1910@gmail.com>
2017-11-27 13:42:13 +0700

    update main

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactComponent } from './contact/contact.component';
+import { OrderComponent } from './order/order.component';
+import { ProductComponent } from './product/product.component';
+import { ProductCategoryComponent } from './product-category/product-category.component';
+import { SlideComponent } from './slide/slide.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { UserComponent } from './user/user.component';
+import { RoleComponent } from './role/role.component';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ContactComponent, OrderComponent, ProductComponent, ProductCategoryComponent, SlideComponent, FeedbackComponent, UserComponent, RoleComponent, HomeComponent, MainComponent]
 })
 export class MainModule { }
